fee8d98da7979f97890ba507aeb887f0545d310b
do-cong-e <congcong4188@naver.com>
2026-01-07 15:07:16 +0900

    튜플연습 이잉기

--- Program.cs
+++ Program.cs
@@ -1,24 +1,37 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net.Cache;
 
-namespace ReadOnlyMethod
+namespace PosisionalPattern
 {
-    struct ACSetting
-    {
-        public double currentInCelsius;
-        public double target;
-
-        public readonly double GetFahrenheit()
-        {
-            //target = currentInCelsius * 1.8 + 32;
-            return currentInCelsius * 1.8 + 32;
-        }
-    }
-
     class MainApp
     {
+        private static double GetDiscountRate(object client)
+        {
+            return client switch
+            {
+                ("학생", int n) when n < 18 => 0.2,
+                ("학생", _) => 0.1,
+                ("일반", int n) when n < 18 => 0.1,
+                ("일반", _) => 0.05,
+                _ => 0,
+            };
+        }       
+        
         static void Main(string[] args)
         {
+            var alice = (job: "학생", age: 17);
+            var bob = (job: "학생", age: 23);
+            var charli = (job: "일반", age: 17);
+            var dave = (job: "일반", age: 21);
+
+            Console.WriteLine($"alice : {GetDiscountRate(alice)}");
+            Console.WriteLine($"alice : {GetDiscountRate(bob)}");
+            Console.WriteLine($"alice : {GetDiscountRate(charli)}");
+            Console.WriteLine($"alice : {GetDiscountRate(dave)}");
 
         }
+
+
     }
 }
