98341329009705319bb565d24550cc05c486193a
do-cong-e <congcong4188@naver.com>
2026-01-07 14:19:13 +0900

    이잉기~

--- Program.cs
+++ Program.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 
-namespace TypeCasting
+namespace ExStructure
 {
-    class Mammal
+    struct Point3D
     {
-        public void nurse()
-        {
-            Console.WriteLine("Nurse()");
-        }
-    }
+        public int x;
+        public int y;
+        public int z;
 
-    class dog : Mammal
-    {
-        public void bark()
-        {
-            Console.WriteLine("Bark()");
-        }
-    }
+        //구조체는 public 으로 선언.. 
 
-    class cat : Mammal
-    {
-        public void meow()
+        public Point3D(int x,int y,int z)
         {
-            Console.WriteLine("Meow()");
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{this.x}, {this.y}, {this.z}");
         }
     }
 
@@ -30,32 +27,21 @@ namespace TypeCasting
     {
         static void Main(string[] args)
         {
-            Mammal mammal = new dog();
-            dog ddog;
+            Point3D p3d1;
+            p3d1.x = 10;
+            p3d1.y = 20;
+            p3d1.z = 40;
 
-            if(mammal is dog)
-            {
-                ddog = (dog)mammal;
-                ddog.bark();
-            }
+            Console.WriteLine(p3d1.ToString());
 
-            Mammal mammal2 = new cat();
+            Point3D p3d2 = new Point3D(100, 200, 300);
+            Point3D p3d3 = p3d2;
+            p3d3.z = 400;
 
-            cat cat2 = mammal2 as cat;
-            if(cat2 !=null)
-            {
-                cat2.meow();
-            }
+            Console.WriteLine(p3d2.ToString());
+            Console.WriteLine(p3d3.ToString());
 
-            cat cat3 = mammal as cat;
-            if(cat3 != null)
-            {
-                cat3.meow();
-            }
-            else
-            {
-                Console.WriteLine("Cat3 is not a cat");
-            }
+            return;
         }
     }
 }
