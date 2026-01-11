e7b0067627af7dcff23d15e30bdc98fad12a20d1
do-cong-e <congcong4188@naver.com>
2026-01-11 18:50:30 +0900

    추상추상

--- Program.cs
+++ Program.cs
@@ -1,119 +1,18 @@
 ﻿using System;
 using System.IO;
 
-namespace IInterface
+namespace chu_sang_class
 {
-    interface ILogger
+    abstract class AbstractBase
     {
-        void WriteLog(string path);
+        public abstract void SomeMathod();
     }
 
-    interface IFormattableLogger : ILogger
+    class Derived : AbstractBase
     {
-        void WriteLog(string format, params Object[] args);
-    }
-
-    class ConsoleLogger :ILogger
-    {
-        public void WriteLog(string Message)
+        public override void SomeMathod()
         {
-            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), Message);
-        }
-    }
-
-    class ConsoleLogger2 : IFormattableLogger
-    {
-        public void WriteLog(string message)
-        {
-            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
-        }
-
-        public void WriteLog(string format, params Object[] args)
-        {
-            String message = String.Format(format, args);
-            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
-        }
-    }
-
-    class FileLogger :ILogger
-    {
-        private StreamWriter writer;
-
-        public FileLogger(string path)
-        {
-            writer = File.CreateText(path);
-            writer.AutoFlush = true;
-        }
-
-        public void WriteLog(string message)
-        {
-            writer.WriteLine("{0} {1}", DateTime.Now.ToShortTimeString(), message);
-        }
-    }
-
-    class ClimateMonitor
-    {
-        private ILogger logger;
-        public ClimateMonitor(ILogger logger)
-        {
-            this.logger = logger;
-        }
-
-        public void start()
-        {
-            while(true)
-            {
-                Console.WriteLine("온도를 입력해주세요. :");
-                string temperature = Console.ReadLine();
-                if(temperature == "")
-                {
-                    break;
-                }
-
-                logger.WriteLog("현재 온도 :" + temperature);
-            }
-        }
-    }
-
-    interface IRunnable
-    {
-        void Run();
-    }
-
-    interface IFlyable
-    {
-        void Fly();
-    }
-
-    class FlyingCar : IRunnable, IFlyable
-    {
-        public void Run()
-        {
-            Console.WriteLine("Run");
-        }
-        public void Fly()
-        {
-            Console.WriteLine("Fly");
-        }
-    }
-
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            //ClimateMonitor monitor = new ClimateMonitor(new FileLogger("MyLog.txt"));
-            //ClimateMonitor monitor = new ClimateMonitor(new ConsoleLogger());
-            //monitor.start();
-
-            FlyingCar car = new FlyingCar();
-            car.Run();
-            car.Fly();
-
-            IRunnable runnable = car as IRunnable;
-            runnable.Run();
-
-            IFlyable flyable = car as IFlyable;
-            flyable.Fly();
+            //something;
         }
     }
 }
