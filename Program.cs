aadc6fcb4b4a08c9553a0917521bb31e848595a1
do-cong-e <congcong4188@naver.com>
2026-01-13 14:19:52 +0900

    자동 프로퍼티

--- Program.cs
+++ Program.cs
@@ -4,52 +4,27 @@ namespace Property
 {
     class BirthdayInfo
     {
-        private string name;
-        private DateTime birthday;
-
-        public string Name
-        {
-            get
-            {
-                return this.name;
-            }
-            set
-            {
-                this.name = value;
-            }
-        }
-
-        public DateTime BirthDay
-        {
-            get
-            {
-                return this.birthday;
-            }
-            set
-            {
-                this.birthday = value;
-            }
-        }
-
+        public string Name { get; set; } = "Unknown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
         public int Age
         {
             get
             {
-                return new DateTime(DateTime.Now.Subtract(birthday).Ticks).Year;
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
             }
         }
-    }
 
+    }
     class MainApp
     {
         static void Main(string[] args)
         {
             BirthdayInfo birth = new BirthdayInfo();
             birth.Name = "쿠우";
-            birth.BirthDay = new DateTime(2009, 12, 04);
+            birth.Birthday = new DateTime(2009, 12, 04);
 
             Console.WriteLine($"Name : {birth.Name}");
-            Console.WriteLine($"Birth day : {birth.BirthDay.ToShortDateString()}");
+            Console.WriteLine($"Birth day : {birth.Birthday.ToShortDateString()}");
             Console.WriteLine($"Age : {birth.Age}");
 
             return;
