50e8a795daf30cb9473f6772f1bf07195bdd630a
do-cong-e <congcong4188@naver.com>
2026-01-15 18:27:19 +0900

    리스트 예제

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ArraySample2
 {
@@ -6,41 +7,35 @@ namespace ArraySample2
     {
         static void Main(string[] args)
         {
-            int[][] jagged = new int[3][];
-            // 행의 갯수만 지정..
-
-            jagged[0] = new int[5]{1,2,3,4,5};
-            jagged[1] = new int[3] {10,20,30};
-            jagged[2] = new int[2] {100,200};
-
-            foreach(int[] arr in jagged)
+            ArrayList list = new ArrayList();
+            for(int i=0;i<5;i++)
             {
-                Console.Write($"Length : {arr.Length}, ");
-                foreach(int element in arr)
-                {
-                    Console.Write($" {element}");
-                }
-                Console.WriteLine("");
-            }
-            Console.WriteLine("-----");
-
-            int[][] jagged2 = new int[2][]
-            {
-                new int[]{1000,2000 },
-                new int[4]{6,7,8,9}
-            };
-
-
-            foreach (int[] arr in jagged2)
-            {
-                Console.Write($"Length : {arr.Length} ,");
-                foreach (int element in arr)
-                {
-                    Console.Write($" {element}");
-                }
-                Console.WriteLine("");
+                list.Add(i);
+                // 리스트 뒤에 추가하는 함수..
             }
 
+            foreach (object obj in list)
+            {
+                Console.Write($"{obj} ");
+            }
+            Console.WriteLine();
+
+            list.Insert(2, 2);
+
+            foreach (object obj in list)
+            {
+                Console.Write($"{obj} ");
+            }
+            Console.WriteLine();
+
+            list.Add("abc");
+            list.Add("def");
+
+            for(int i=0;i<list.Count;i++)
+            {
+                Console.Write($"{list[i]} ");
+            }
+            Console.WriteLine();
         }
     }
 }
