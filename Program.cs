1430c1dc1902ec389291f27e2c13332923551c51
do_cong_ee <congcong4188@naver.com>
2026-01-15 19:07:09 +0900

    quue

--- Program.cs
+++ Program.cs
@@ -1,41 +1,23 @@
 ﻿using System;
 using System.Collections;
 
-namespace ArraySample2
+namespace UsingQueue
 {
     class MainApp
     {
         static void Main(string[] args)
         {
-            ArrayList list = new ArrayList();
-            for(int i=0;i<5;i++)
+            Queue que = new Queue();
+            que.Enqueue(1);
+            que.Enqueue(2);
+            que.Enqueue(3);
+            que.Enqueue(4);
+            que.Enqueue(5);
+
+            while(que.Count>0)
             {
-                list.Add(i);
-                // 리스트 뒤에 추가하는 함수..
+                Console.WriteLine(que.Dequeue());
             }
-
-            foreach (object obj in list)
-            {
-                Console.Write($"{obj} ");
-            }
-            Console.WriteLine();
-
-            list.Insert(2, 2);
-
-            foreach (object obj in list)
-            {
-                Console.Write($"{obj} ");
-            }
-            Console.WriteLine();
-
-            list.Add("abc");
-            list.Add("def");
-
-            for(int i=0;i<list.Count;i++)
-            {
-                Console.Write($"{list[i]} ");
-            }
-            Console.WriteLine();
         }
     }
 }
