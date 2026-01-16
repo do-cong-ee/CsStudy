459af80eb77b87de5c5ab5c018313ec50e498f88
do_cong_ee <congcong4188@naver.com>
2026-01-16 18:50:00 +0900

    ㅇㅇ

--- Program.cs
+++ Program.cs
@@ -1,41 +1,54 @@
 ﻿using System;
 using System.Collections;
 
-namespace MyProgram
+namespace IndexerEx
 {
+    class MyList
+    {
+        private int[] array;
+
+        public MyList()
+        {
+            array = new int[3];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<int>(ref array, index + 1);
+                    Console.WriteLine("Array Resized : {0}", array.Length);
+                }
+                array[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+    }
 
     class MainApp
     {
-        static int Recursion(string str, int l,int r,ref int num_of_recursion)
-        {
-            if (l >= r) return 1;
-            else if (str[l] != str[r]) return 0;
-            else{
-                num_of_recursion++;
-                return  Recursion(str, l + 1, r - 1, ref num_of_recursion);
-            }
-        }
-        static int Palindrom(string str, ref int num_of_recursion)
-        {
-            num_of_recursion++;
-            return Recursion(str, 0, str.Length - 1, ref num_of_recursion);
-        }
         static void Main(string[] args)
         {
-            Int32 num;
-            num = int.Parse(Console.ReadLine());
-            string str="";
-            int num_of_recursion=0;
-            for(int i=0;i<num;i++)
+            MyList list = new MyList();
+            for(int i=0;i<5;i++)
             {
-                str = Console.ReadLine();
-                num_of_recursion = 0;
-                Console.Write("{0} ",Palindrom(str, ref num_of_recursion));
-                Console.WriteLine("{0}", num_of_recursion);
-                //레퍼런스 전달할때는 함수의 호출에서도 ref 키워드 써줘야..
-                
+                list[i] = i;
             }
-            
+
+            for(int i=0;i<list.Length;i++)
+            {
+                Console.WriteLine(list[i]);
+            }
         }
     }
 }
