86e1740081c46de9a0e96d68dcce5c6aa80c5df4
do_cong_ee <congcong4188@naver.com>
2026-01-18 16:55:37 +0900

    이잉기

--- Program.cs
+++ Program.cs
@@ -2,27 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace KillingProgram
+namespace Throw
 {
     class MainApp
     {
+       static void DoSomeThing(int arg)
+        {
+            if (arg < 10)
+                Console.WriteLine($"arg : {arg}");
+            else
+                throw new Exception("arg가 10보다 큽니다");
+        }
+
         static void Main(string[] args)
         {
-            int[] arr = new int[3] { 1, 2, 3 };
-
             try
             {
-                for (int i = 0; i < 5; i++)
-                {
-                    Console.WriteLine($"{arr[i]}");
-                }
+                DoSomeThing(1);
+                DoSomeThing(3);
+                DoSomeThing(5);
+                DoSomeThing(7);
+                DoSomeThing(9);
+                DoSomeThing(11);
+                DoSomeThing(13); // 26행에서 예외 생겨서 여기는 실행안됨.
             }
-            catch (IndexOutOfRangeException e)
+            catch (Exception e)
             {
-                Console.WriteLine($"예외가 발생했습니다 : {e.Message}");
+                //Method 에서 exception 을 throw 하면 call 한 위치에서 catch 한다.
+                Console.WriteLine(e.Message);
             }
-
-            Console.WriteLine("종료");
         }
     }
 }
