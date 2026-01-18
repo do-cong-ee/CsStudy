266cafe5791dd535bbc8807000dd85e6ab4bc0e7
do_cong_ee <congcong4188@naver.com>
2026-01-18 17:08:04 +0900

    커밋이잉기

--- Program.cs
+++ Program.cs
@@ -2,34 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Throw
+namespace ThrowException
 {
     class MainApp
     {
-       static void DoSomeThing(int arg)
-        {
-            if (arg < 10)
-                Console.WriteLine($"arg : {arg}");
-            else
-                throw new Exception("arg가 10보다 큽니다");
-        }
-
         static void Main(string[] args)
         {
             try
             {
-                DoSomeThing(1);
-                DoSomeThing(3);
-                DoSomeThing(5);
-                DoSomeThing(7);
-                DoSomeThing(9);
-                DoSomeThing(11);
-                DoSomeThing(13); // 26행에서 예외 생겨서 여기는 실행안됨.
+                int? a=null; //nullable 
+                int b = a ?? throw new ArgumentNullException();
             }
-            catch (Exception e)
+            catch (ArgumentNullException e)
             {
-                //Method 에서 exception 을 throw 하면 call 한 위치에서 catch 한다.
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"{e}");
+            }
+
+            try
+            {
+                int[] array = new int[] { 1, 2, 3 };
+                int index = 4;
+                int value = array[
+                    index >= 0 && index < 4 ? index : throw new IndexOutOfRangeException()
+                    ];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
