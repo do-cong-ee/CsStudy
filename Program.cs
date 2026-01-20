4855d88434486f959d76c811151d28aef2d216e5
do_cong_ee <congcong4188@naver.com>
2026-01-20 20:08:07 +0900

    삼겹살먹고싶다.

--- Program.cs
+++ Program.cs
@@ -1,34 +1,58 @@
 ﻿using System;
 
-namespace Dae_ri_ja
+namespace UsingCallBack
 {
-    delegate int MyDelegate(int a, int b);
-    class Calculator
-    {
-        public int Plus(int a,int b)
-        {
-            return a + b;
-        }
-        public int Minus(int a,int b)
-        {
-            return a - b;
-        }
-    }
+    delegate int Compare(int a, int b);
 
     class MainApp
     {
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+            {
+                return 1;
+            }
+            else if (a == b)
+            {
+                return 0;
+            }
+            else
+                return -1;
+        }
+
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        static void BubbleSort(int[] arr, Compare Comparer)
+        {
+            int i = 0, j = 0, temp = 0;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                for (j = 0; j < arr.Length - (i + 1); i++)
+                {
+                    if (Comparer(arr[j], arr[j + 1]) > 0)
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Calculator Calc = new Calculator();
-            MyDelegate Callback;
-
-            Callback = new MyDelegate(Calc.Plus);
-            Console.WriteLine(Callback(3, 4));
-
-            Callback = new MyDelegate(Calc.Minus);
-            Console.WriteLine(Callback(3, 4));
+            int[] array = new int[] { 3, 7, 4, 2, 10 };
+            Console.WriteLine("Ascending");
+            BubbleSort(array, new Compare(AscendCompare));
         }
     }
-
-  
 }
