29d70f93264ffb7b5766a84905a59a5e382257b7
do_cong_ee <congcong4188@naver.com>
2026-01-22 16:00:24 +0900

    dd

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace Rambda_sik
 {
     delegate int Calculator(int a, int b);
-
+    delegate void DoSomeThing();
     class MainApp
     {
         static int Calculator2(int a, int b)
@@ -22,6 +22,13 @@ namespace Rambda_sik
 
             Console.WriteLine(calc2(120, 20));
 
+            DoSomeThing doit = () =>
+            {
+                Console.WriteLine("do some Thing");
+                Console.WriteLine("like this");
+            };
+
+            doit();
 
             return;
         }
