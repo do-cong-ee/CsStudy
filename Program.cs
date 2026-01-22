8574399b6563901fca731bbda20fa287e8eb47ea
do_cong_ee <congcong4188@naver.com>
2026-01-22 19:37:32 +0900

    sw

--- Program.cs
+++ Program.cs
@@ -1,60 +1,22 @@
 ﻿using System;
+using System.Linq;
 
-namespace TypeCasting
+namespace FromEx
 {
-    class Mammal
-    {
-        public void nurse()
-        {
-            Console.WriteLine("Nurse()");
-        }
-    }
-
-    class dog : Mammal
-    {
-        public void bark()
-        {
-            Console.WriteLine("Bark()");
-        }
-    }
-
-    class cat : Mammal
-    {
-        public void meow()
-        {
-            Console.WriteLine("Meow()");
-        }
-    }
-
     class MainApp
     {
         static void Main(string[] args)
         {
-            Mammal mammal = new dog();
-            dog ddog;
+            int[] numbers = new int[] { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
 
-            if(mammal is dog)
-            {
-                ddog = (dog)mammal;
-                ddog.bark();
-            }
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n
+                         select n;
 
-            Mammal mammal2 = new cat();
-
-            cat cat2 = mammal2 as cat;
-            if(cat2 !=null)
+            foreach (var element in result)
             {
-                cat2.meow();
-            }
-
-            cat cat3 = mammal as cat;
-            if(cat3 != null)
-            {
-                cat3.meow();
-            }
-            else
-            {
-                Console.WriteLine("Cat3 is not a cat");
+                Console.WriteLine($"{element}");
             }
         }
     }
