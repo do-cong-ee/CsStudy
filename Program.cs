d0a8c53bfe5bedf0b10947b30c39df4a88fbe42f
do_cong_ee <congcong4188@naver.com>
2026-01-22 17:23:55 +0900

    xx

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace using_action
             Action<string> act = (string message) => { Console.WriteLine(message); };
 
             act("Hello World!");
+
+            int result=0;
+
+            Action<int, int> act2 = (int a, int b) => { result = a + b; };
+            act2(20, 20);
+            Console.WriteLine(result);
+
+            Action action = new action(delegate () { Console.WriteLine(a * a)});
+
+            Action action = (int a) => { Console.WriteLine(a*a)};
         }
     }
 }
+
