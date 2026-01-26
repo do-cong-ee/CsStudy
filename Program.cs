4c33287cf6e94c376e5faaa3fb473c2667355a29
do_cong_ee <congcong4188@naver.com>
2026-01-26 20:12:01 +0900

    ㅇㅇ

--- Program.cs
+++ Program.cs
@@ -1,42 +1,27 @@
 ﻿using System;
-using System.Dynamic;
-using System.Reflection;
-using System.Reflection.Emit;
 
-namespace EmitTest
+namespace MyAttribute
 {
-    public class MainApp
+    class MyClass
     {
-        public static void Main(string[] args)
+        [Obsolete("OldMethod 는 버려졌읍니다, NewMethod()를 이용하셔요 아이고난1 아이고난2 뭉탱이")]
+        public void OldMethod()
         {
-            AssemblyBuilder newAssembly =
-                AssemblyBuilder.DefineDynamicAssembly(
-                    new AssemblyName("CalculatorAssembly"),
-                    AssemblyBuilderAccess.Run); // 어샘블리 생성 -> 이제 모듈 생성할차례
+            Console.WriteLine("I am Old");
+        }
 
-            ModuleBuilder newModule = newAssembly.DefineDynamicModule("Calculator"); // 모듈 -> 이제 class 타입 생성할차례
-
-            TypeBuilder newType = newModule.DefineType("Sum1To100"); //class -> 이제 메소드 생성할 차례
-
-            MethodBuilder newMethod = newType.DefineMethod("Calculate",MethodAttributes.Public,typeof(int),new Type[0]);
-
-            ILGenerator generator = newMethod.GetILGenerator(); //IL 명령어 생성기 
-
-            generator.Emit(OpCodes.Ldc_I4, 1);
-
-            for(int i=2;i<=100;i++)
-            {
-                generator.Emit(OpCodes.Ldc_I4, i);
-                generator.Emit(OpCodes.Add);
-            }
-
-            generator.Emit(OpCodes.Ret); //반환 
-            newType.CreateType(); //작성을 다함 
-
-            object sum1To100 = Activator.CreateInstance(newType);
-            Type sumType = sum1To100.GetType();
-            MethodInfo calculateMethod = sumType.GetMethod("Calculate");
-            Console.WriteLine(calculateMethod.Invoke(sum1To100, null));
+        public void NewMethod()
+        {
+            Console.WriteLine("I am new");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MyClass myclass = new MyClass();
+            myclass.OldMethod();
+            myclass.NewMethod();
         }
     }
 }
