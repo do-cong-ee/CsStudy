56e520c37db84fce7bc7d05ccb88fbd2bfcce430
do_cong_ee <congcong4188@naver.com>
2026-01-26 20:57:01 +0900

    덕타이핑 이잉기~

--- Program.cs
+++ Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
 
-namespace MyDynamic
+namespace DuckTyping
 {
-    class MyClass
+    class Duck
     {
-        public void FuncAAA()
+        public void Walk()
         {
-            //do not thing
+            Console.WriteLine(this.GetType() + ".walk");
+        }
+        public void Swim()
+        {
+            Console.WriteLine(this.GetType() + ".swim");
+        }
+        public void Quack()
+        {
+            Console.WriteLine(this.GetType() + ".quack");
+        }
+    }
+
+    class Mallard : Duck
+    {
+    }
+
+    class Robot
+    {
+        public void Walk()
+        {
+            Console.WriteLine(this.GetType() + ".walk");
+        }
+        public void Swim()
+        {
+            Console.WriteLine(this.GetType() + ".swim");
+        }
+        public void Quack()
+        {
+            Console.WriteLine(this.GetType() + ".quack");
         }
     }
 
@@ -16,9 +42,17 @@ namespace MyDynamic
     {
         static void Main(string[] args)
         {
-            dynamic obj = new MyClass();
-            obj.FuncAAA();
-            obj.FuncBBB();
+            dynamic[] arr = new dynamic[3] { new Duck(), new Mallard { }, new Robot() };
+            //Duck[] arr2 = new Duck[3] { new Duck(), new Mallard { }, new Robot() };
+            foreach (dynamic duck in arr)
+            {
+                Console.WriteLine(duck.GetType());
+                duck.Walk();
+                duck.Swim();
+                duck.Quack();
+
+                Console.WriteLine();
+            }
         }
     }
 }
