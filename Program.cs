898ab8bfada941651d4d294c14951babb865aa6e
do_cong_ee <congcong4188@naver.com>
2026-01-26 17:50:35 +0900

    이잉기~

--- Program.cs
+++ Program.cs
@@ -29,6 +29,55 @@ namespace GetType
                 BindingFlags.Public |
                 BindingFlags.Static |
                 BindingFlags.Instance);
+
+            foreach (FieldInfo field in fieldinfos)
+            {
+                String accessLevel = "protected";
+                if (field.IsPublic) accessLevel = "public";
+                else if (field.IsPrivate) accessLevel = "private";
+
+                Console.WriteLine("Access : {0}, Type : {1}, Name:{2}",
+                    accessLevel, field.FieldType.Name, field.Name);
+            }
+
+            Console.WriteLine();
         }
+        
+        static void PrintMethods(Type type)
+        {
+            Console.WriteLine("--------------Methods---------------");
+            MethodInfo[] methods = type.GetMethods();
+
+            foreach(MethodInfo method in methods)
+            {
+                Console.Write("Type : {0}, Name :{1}, Parameter :",
+                    method.ReturnType.Name, method.Name);
+
+                ParameterInfo[] args = method.GetParameters();
+
+                for(int i=0;i<args.Length;i++)
+                {
+                    Console.Write("{0}", args[i].ParameterType.Name);
+                    if(i<args.Length-1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    
+        static void Main(string[] args)
+        {
+            int a=0;
+            Type type = a.GetType();
+
+            PrintFields(type);
+            PrintInterfaces(type);
+            PrintMethods(type);
+        }
+    
     }
+
 }
