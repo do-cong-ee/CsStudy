5c7bc44e760df1b1a6eaaf9f88589ef6f5898099
do_cong_ee <congcong4188@naver.com>
2026-01-27 17:25:49 +0900

    d

--- Program.cs
+++ Program.cs
@@ -2,44 +2,58 @@
 using System.Linq;
 using System.IO;
 
-namespace Dir
+namespace Touch
 {
     class MainApp
     {
+        static void OnWrongPathType(string type)
+        {
+            Console.WriteLine($"{type} is wrong");
+            return;
+        }
+
         static void Main(string[] args)
         {
-            string directory;
-            if (args.Length < 1)
-                directory = ".";
+            if(args.Length==0)
+            {
+                Console.WriteLine(
+                    "Useage : Touch.exe <path> [Type:File/Diretory]"
+                    );
+            }
+
+            string path = args[0];
+            string type = "File";
+            if(args.Length>1)
+            {
+                type = args[1];
+            }
+
+            if(File.Exists(path) || Directory.Exists(path))
+            {
+                if (type == "File")
+                    File.SetLastWriteTime(path, DateTime.Now);
+                else if (type == "Directory")
+                    Directory.SetLastWriteTime(path, DateTime.Now);
+                else
+                {
+                    OnWrongPathType(path);
+                    return;
+                }
+                Console.WriteLine($"Updated {path} : {type}");
+            }
             else
-                directory = args[0];
-
-            Console.WriteLine($"{directory} directory Info");
-            Console.WriteLine("-Directorise : ");
-
-            var directories = (from dir in Directory.GetDirectories(directory)
-                               let info = new DirectoryInfo(dir)
-                               select new
-                               {
-                                   Name = info.Name,
-                                   Attributes = info.Attributes
-                               }).ToList();
-
-            foreach (var d in directories)
-                Console.WriteLine($"{d.Name} : {d.Attributes}");
-
-            Console.WriteLine("-Files");
-            var files = (from file in Directory.GetFiles(directory)
-                         let info = new FileInfo(file)
-                         select new
-                         {
-                             Name = info.Name,
-                             FileSize = info.Length,
-                             Attributes = info.Attributes
-                         }).ToList();
-
-            foreach (var f in files)
-                Console.WriteLine($"{f.Name} : {f.FileSize}, {f.Attributes}");
+            {
+                if (type == "File")
+                    File.Create(path).Close();
+                else if (type == "Directory")
+                    Directory.CreateDirectory(path);
+                else
+                {
+                    OnWrongPathType(path);
+                    return;
+                }
+                Console.WriteLine($"Creat {path} : {type}");
+            }
         }
     }
 }
