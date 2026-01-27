72dc2f0a2819933a279a03dffcd12c3f57cf9c13
do_cong_ee <congcong4188@naver.com>
2026-01-27 16:00:59 +0900

    이잉기

--- Program.cs
+++ Program.cs
@@ -1,58 +1,45 @@
 ﻿using System;
+using System.Linq;
+using System.Io;
 
-namespace DuckTyping
+namespace Dir
 {
-    class ori
-    {
-        public void Walk()
-        {
-            Console.WriteLine(this.GetType() + ".walk");
-        }
-        public void Swim()
-        {
-            Console.WriteLine(this.GetType() + ".swim");
-        }
-        public void Quack()
-        {
-            Console.WriteLine(this.GetType() + ".quack");
-        }
-    }
-
-    class Mallard : ori
-    {
-    }
-
-    class Robot
-    {
-        public void Walk()
-        {
-            Console.WriteLine(this.GetType() + ".walk");
-        }
-        public void Swim()
-        {
-            Console.WriteLine(this.GetType() + ".swim");
-        }
-        public void Quack()
-        {
-            Console.WriteLine(this.GetType() + ".quack");
-        }
-    }
-
     class MainApp
     {
         static void Main(string[] args)
         {
-            dynamic[] arr = new dynamic[3] { new ori(), new Mallard { }, new Robot() };
-            //Duck[] arr2 = new Duck[3] { new Duck(), new Mallard { }, new Robot() };
-            foreach (dynamic duck in arr)
-            {
-                Console.WriteLine(duck.GetType());
-                duck.Walk();
-                duck.Swim();
-                duck.Quack();
+            string directory;
+            if (args.Length < 1)
+                directory = ".";
+            else
+                directory = args[0];
 
-                Console.WriteLine();
-            }
+            Console.WriteLine($"{directory} directory Info");
+            Console.WriteLine("-Directorise : ");
+
+            var directories = (from dir in Directory.GetDirectories(directory)
+                               let info = new DirectoryInfo(dir)
+                               select new
+                               {
+                                   Name = info.name,
+                                   Attributes = info.Attributes
+                               }).ToList();
+
+            foreach (var d in directories)
+                Console.WriteLine($"{d.Name} : {d.Attributes}");
+
+            Console.WriteLine("-Files");
+            var files = (from file in Directory.GetFiles(directory)
+                         let info = new FileInfo(file)
+                         select new
+                         {
+                             Name = info.Name,
+                             FileSize = info.Length,
+                             Attributes = info.Attributes
+                         }).ToList();
+
+            foreach (var f in files)
+                Console.WriteLine($"{f.name} : {f.FileSize}, {f.Attributes}");
         }
     }
 }
