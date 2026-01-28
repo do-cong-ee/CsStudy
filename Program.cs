b9460a22ff7f155b21bf197d5ab306632bc482e7
do_cong_ee <congcong4188@naver.com>
2026-01-28 17:36:48 +0900

    이잉기~

--- Program.cs
+++ Program.cs
@@ -1,45 +1,36 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text.Json.Serialization;
-#pragma warning disable SYSLIB0011
+using System.Threading;
 
-
-namespace Serialization
-    
+namespace BasicThreading
 {
     class MainApp
     {
-        [Serializable]
-        class NameCard
+        static void DoSomeThing()
         {
-            public string Name;
-            public string Phone;
-            public int age;
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine($"DoSomeThing : {i}");
+                //Thread.Sleep(3);
+            }
         }
+
+
         static void Main(string[] args)
         {
-            using (Stream ws = new FileStream("a.dat", FileMode.Create))
+            Thread t1 = new Thread(new ThreadStart(BasicThreading.MainApp.DoSomeThing));
+            Console.WriteLine("Starting Thread...");
+            t1.Start();
+
+            for (int i = 0; i < 500; i++)
             {
-                BinaryFormatter serializer = new BinaryFormatter();
-
-                NameCard nc = new NameCard();
-                nc.Name = "대식이";
-                nc.Phone = "010-0000-0000";
-                nc.age = 2000;
-
-                serializer.Serialize(ws, nc);
+                Console.WriteLine($"Main : {i}");
+                //Thread.Sleep(2);
             }
 
-            using Stream rs = new FileStream("a.dat", FileMode.Open);
-            BinaryFormatter deserializer = new BinaryFormatter();
+            Console.WriteLine("Waiting Until Thread Stop.....");
+            t1.Join();
 
-            NameCard nc2;
-            nc2 = (NameCard)deserializer.Deserialize(rs);
-
-            Console.WriteLine(nc2.Name);
-            Console.WriteLine(nc2.Phone);
-            Console.WriteLine(nc2.age);
+            Console.WriteLine("Finished");
         }
     }
 }
