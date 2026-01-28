2ae89d9b8b0e5ed4a2ed9150d5bb72f208ef0d78
do_cong_ee <congcong4188@naver.com>
2026-01-28 18:01:58 +0900

    ./net10 에 호환되는 방법....

--- Program.cs
+++ Program.cs
@@ -13,21 +13,22 @@ namespace AbortingThread
             this.count = count;
         }
 
-        public void KeepAlive()
+        public void KeepAlive(CancellationToken token)
         {
             try
             {
-                while(this.count>0)
+                while (this.count > 0)
                 {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Cancellation requested: stopping gracefully.");
+                        break;  // 반복문 벗어나서 정리
+                    }
+
                     Console.WriteLine($"{count--} left");
                     Thread.Sleep(10);
                 }
                 Console.WriteLine("Count : 0");
-            }
-            catch(ThreadAbortException e)
-            {
-                Console.WriteLine(e);
-                Thread.ResetAbort();
             }
             finally //복습, 예외가 생기든 안생기든 무조껀 실행하는 코드
             {
@@ -41,7 +42,17 @@ namespace AbortingThread
         static void Main(string[] args)
         {
             SideTask task = new SideTask(100);
-            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+
+           
+
+            // .NET10 에서 abort 대신 쓰는거, 취소 토큰 소스 생성
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            //Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            //t1.IsBackground = false;
+
+            Thread t1 = new Thread(() => task.KeepAlive(token));
             t1.IsBackground = false;
 
             Console.WriteLine("Starting Thread...");
@@ -50,8 +61,10 @@ namespace AbortingThread
             Thread.Sleep(100);
 
             Console.WriteLine("Aborting Thread...");
-            t1.Abort();
+            cts.Cancel();
+            //t1.Abort();
             //  t1.Abort(); 은 .NET10 에서 사라진기능 실행하고싶다면 .NET9 에서 실행하기
+
 
             Console.WriteLine("Wating until thread stops...");
             t1.Join();
