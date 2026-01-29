8d10246b82a00103b69357cad2c3b50e08348b9f
do_cong_ee <congcong4188@naver.com>
2026-01-29 17:09:44 +0900

    dd

--- Program.cs
+++ Program.cs
@@ -1,75 +1,30 @@
-﻿#pragma warning disable SYSLIB0006
-
-using System;
+﻿using System;
 using System.Threading;
 
-namespace AbortingThread
+namespace Synchronize
 {
-    class SideTask
+    class Counter
     {
-        int count;
-        public SideTask(int count)
+        const int LOOP_COUNT = 10000;
+        readonly object thislock;
+        private int count;
+
+        public int Count
         {
-            this.count = count;
+            get { return count; }
         }
 
-        public void KeepAlive(CancellationToken token)
+        public Counter()
         {
-            try
-            {
-                while (this.count > 0)
-                {
-                    if (token.IsCancellationRequested)
-                    {
-                        Console.WriteLine("Cancellation requested: stopping gracefully.");
-                        break;  // 반복문 벗어나서 정리
-                    }
-
-                    Console.WriteLine($"{count--} left");
-                    Thread.Sleep(10);
-                }
-                Console.WriteLine("Count : 0");
-            }
-            finally //복습, 예외가 생기든 안생기든 무조껀 실행하는 코드
-            {
-                Console.WriteLine("Clearing resource...");
-            }
+            thislock = new object();
+            count = 0;
         }
-    }
 
-    class MainApp
-    {
-        static void Main(string[] args)
+        public void Increase()
         {
-            SideTask task = new SideTask(100);
+            this.LOOP_COUNT = 20;
 
-           
-
-            // .NET10 에서 abort 대신 쓰는거, 취소 토큰 소스 생성
-            CancellationTokenSource cts = new CancellationTokenSource();
-            CancellationToken token = cts.Token;
-
-            //Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
-            //t1.IsBackground = false;
-
-            Thread t1 = new Thread(() => task.KeepAlive(token));
-            t1.IsBackground = false;
-
-            Console.WriteLine("Starting Thread...");
-            t1.Start();
-
-            Thread.Sleep(100);
-
-            Console.WriteLine("Aborting Thread...");
-            cts.Cancel();
-            //t1.Abort();
-            //  t1.Abort(); 은 .NET10 에서 사라진기능 실행하고싶다면 .NET9 에서 실행하기
-
-
-            Console.WriteLine("Wating until thread stops...");
-            t1.Join();
-
-            Console.WriteLine("Finished");
+            int loopCount = LOOP_COUNT;
         }
     }
 }
