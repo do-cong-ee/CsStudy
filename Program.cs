ca670fd13037011cdcb90b546223e9b15db5c7a3
do_cong_ee <congcong4188@naver.com>
2026-02-02 15:48:41 +0900

    윈포밍프로젝트파일설정

--- Program.cs
+++ Program.cs
@@ -1,61 +1,12 @@
 ﻿using System;
 
-namespace TypeCasting
+namespace SimpleWindow
 {
-    class Mammal
-    {
-        public void nurse()
-        {
-            Console.WriteLine("Nurse()");
-        }
-    }
-
-    class dog : Mammal
-    {
-        public void bark()
-        {
-            Console.WriteLine("Bark()");
-        }
-    }
-
-    class cat : Mammal
-    {
-        public void meow()
-        {
-            Console.WriteLine("Meow()");
-        }
-    }
-
-    class MainApp
+    class MainApp : System.Windows.Forms.Form
     {
         static void Main(string[] args)
         {
-            Mammal mammal = new dog();
-            dog ddog;
-
-            if(mammal is dog)
-            {
-                ddog = (dog)mammal;
-                ddog.bark();
-            }
-
-            Mammal mammal2 = new cat();
-
-            cat cat2 = mammal2 as cat;
-            if(cat2 !=null)
-            {
-                cat2.meow();
-            }
-
-            cat cat3 = mammal as cat;
-            if(cat3 != null)
-            {
-                cat3.meow();
-            }
-            else
-            {
-                Console.WriteLine("Cat3 is not a cat");
-            }
+            System.Windows.Forms.Application.Run(new MainApp());
         }
     }
 }
