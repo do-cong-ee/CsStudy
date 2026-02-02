6b25fb74e2f7373c30c09d4058f533d938f1fe13
do_cong_ee <congcong4188@naver.com>
2026-02-02 17:41:15 +0900

    이잉기~

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace MessageFilter
+namespace FormEvent
 {
-    class MassageFilter : IMessageFilter
-    {
-        public bool PreFilterMessage(ref Message m)
-        {
-            if (m.Msg == 0x0F || m.Msg == 0xA0 || m.Msg == 0x200 || m.Msg == 0x113)
-            {
-               return false;
-            }
-            Console.WriteLine($"{m.ToString()} : {m.Msg}");
-
-            if (m.Msg == 0x201)
-                Application.Exit();
-
-            return true;
-        }
-    }
-
     class MainApp : Form
     {
+        public void MyMouseHandler(object sender , MouseEventArgs e)
+        {
+            //sender 어떤 객체가 이벤트 처리기를 호출 하였는지 -> Button? 
+            // e -> 이벤트처리기. 
+            Console.WriteLine($"Sender : {((Form)sender).Text}");
+            Console.WriteLine($"X : {e.X}, y:{e.Y}");
+            Console.WriteLine($"Button : {e.Button}, Click : {e.Clicks}");
+        }
+
+        public MainApp (string title)
+        {
+            this.Text = title;
+            this.MouseDown += new MouseEventHandler(MyMouseHandler);
+        }
+        
         static void Main(string[] args)
         {
-            Application.AddMessageFilter(new MessageFilter.MassageFilter());
-            Application.Run(new MainApp());
+            Application.Run(new MainApp("Mouse Event Test"));
         }
+
     }
+
+    
 }
