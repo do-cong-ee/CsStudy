312e3a0660bf2afe7f2f9e03a6ac76a64108996b
do_cong_ee <congcong4188@naver.com>
2026-02-04 17:54:31 +0900

    잉기

--- Program.cs
+++ Program.cs
@@ -2,18 +2,18 @@
 
 namespace EventTest
 {
-    delegate void EventHandler(string message);
+    delegate void EventHandler(string message); //일단 대리자 하나 만들어 
 
-    class MyNotifier
+    class MyNotifier //클래스 생성 
     {
-        public event EventHandler SomethingHappened;
+        public event EventHandler SomethingHappened; // 만든 대리자를 형식으로 하는 이벤트 리스너 하나 만들어
         public void DoSomething(int number)
         {
             int temp = number % 10;
 
             if (temp != 0 && temp % 3 == 0)
             {
-                SomethingHappened(String.Format("{0} : 짝", number));
+                SomethingHappened(String.Format("{0} : 짝", number)); //조건이 맞다면 ->delegate 로 생성된 이벤트 처리 함수 call 한다..
             }
         }
     }
