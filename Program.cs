6361552f3ff6028066d4362e077fbd18ee73d2f7
do_cong_ee <congcong4188@naver.com>
2026-03-14 16:46:02 +0900

    커밋잉기

--- Program.cs
+++ Program.cs
@@ -1,75 +1,31 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+using System.Collections;
 
-namespace EchoServer
+namespace Yield
 {
+    class MyEnumerator
+    {
+        int[] numbers = { 1, 2, 3, 4 };
+        public IEnumerator GetEnumerator()
+        {
+            yield return numbers[0];
+            yield return numbers[1];
+            yield return numbers[2];
+            yield break; //GetEnumerator() 을 종료시킨다.
+            yield return numbers[3];
+
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            if(args.Length<1)
+            var obj = new MyEnumerator();
+            foreach(int i in obj)
             {
-                Console.WriteLine("사용법 : {0} <Bind IP>", Process.GetCurrentProcess().ProcessName);
-                return;
+                Console.WriteLine(i);
             }
-
-            string bindIP = args[0];
-            const int bindPort = 5425;
-            TcpListener server = null;
-
-            try
-            {
-                IPEndPoint localAddress =
-                    new IPEndPoint(IPAddress.Parse(bindIP), bindPort);
-
-                server = new TcpListener(localAddress);
-                //TcpListener 은 서버에서 만 쓴다..
-
-                server.Start();
-                Console.WriteLine("메아리 서버 시작...");
-
-                while(true)
-                {
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("클라이언트 접속 : {0}", ((IPEndPoint)client.Client.RemoteEndPoint).ToString());
-
-                    NetworkStream stream = client.GetStream();
-
-                    int length;
-                    string data = null;
-                    byte[] bytes = new byte[256];
-                
-                    while((length = stream.Read(bytes,0,bytes.Length))!=0)
-                    {
-                        data = Encoding.Default.GetString(bytes, 0, length); 
-                        //송신된 데이터를 byte -> string 
-                        Console.WriteLine(String.Format("수신 : {0}", data));
-                        // 수신된 데이터 출력..
-
-
-                        byte[] msg = Encoding.Default.GetBytes(data);
-                        stream.Write(msg, 0, msg.Length);
-                        
-                        Console.WriteLine(String.Format("송신 : {0}", data));
-                    }
-
-                    stream.Close();
-                    client.Close();
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine(e);
-            }
-            finally
-            {
-                server.Stop();
-            }
-
-            Console.WriteLine("서버를 종료합니다")
         }
     }
 }
