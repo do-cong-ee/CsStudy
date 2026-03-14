f9068e61125f18353a3c19aedc7c22a2922390ce
do_cong_ee <congcong4188@naver.com>
2026-03-14 16:48:19 +0900

    dd

--- Program.cs
+++ Program.cs
@@ -1,31 +1,10 @@
 ﻿using System;
 using System.Collections;
 
-namespace Yield
+namespace Enumerable
 {
-    class MyEnumerator
+    class MyList : IEnumerable, IEnumerator
     {
-        int[] numbers = { 1, 2, 3, 4 };
-        public IEnumerator GetEnumerator()
-        {
-            yield return numbers[0];
-            yield return numbers[1];
-            yield return numbers[2];
-            yield break; //GetEnumerator() 을 종료시킨다.
-            yield return numbers[3];
 
-        }
-    }
-
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            var obj = new MyEnumerator();
-            foreach(int i in obj)
-            {
-                Console.WriteLine(i);
-            }
-        }
     }
 }
